860f9c4fb18fbeb6c7af811c14957d129f658ee1
rma-toni <rmaflores.sw@gmail.com>
2024-01-21 13:50:54 -0300

    Add snipetcode of promises

---
+++ js/promises.js
@@ -0,0 +1,21 @@
+//promise
+
+const cows = 9;
+
+//creamos una promesa, lleva de argumentos una funcion que recibe como parametros
+//un resolve y un reject.
+const countCows = new Promise(function (resolve, reject){
+    if (cows > 10) {
+        resolve (`We have ${cows} cows on the farm`);
+    } else {
+        reject ("There is no cows on the farm");
+    }
+});
+
+//con them obtenemos el valor si la promesa es verdadera
+//y con catch obtenemos la respuesta si la promesa es falsa
+countCows.then(function(result){
+    console.log(result);
+}).catch(function(error){
+    console.log(error);
+})
