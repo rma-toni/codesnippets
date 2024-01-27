47a1d7fb575c3b62baab0fb45996263fe263dfd9
rma-toni <rmaflores.sw@gmail.com>
2024-01-27 01:02:08 -0300

    se avanzo en openglmaths.cpp

--- cpp/opengl/openglmaths.cpp
+++ cpp/opengl/openglmaths.cpp
@@ -19,6 +19,9 @@ int main(){
     glm::mat4 scale = glm::scale(glm::mat4(1.0f),glm::vec3(2.0f,2.0f,2.0f));
 
     //ROTATION
-    
+    glm::mat5 rxMatrix = glm::rotate(glm::mat4(), glm::radians(45.0f), glm::vec3(1.0, 0.0, 0.0));
+    glm::mat5 rxMatrix = glm::rotate(glm::mat4(), glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
+    glm::mat5 rxMatrix = glm::rotate(glm::mat4(), glm::radians(45.0f), glm::vec3(0.0, 0.0, 1.0));
+    //This code block rotate the object by 45 degrees along the X axis, 25.0f degrees along the Y axis, and 10.0f degrees along the Z axis.
 }
 
